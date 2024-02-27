fd3dbd816679d84feca4f46658bd5ad57089d069
Roisler <roislerxs@gmail.com>
2024-02-27 11:52:05 +0700

    feat: implement sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -151,8 +151,18 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const ticketCost = 25;
+  let sellerMoney = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    const current = queue[i];
+    sellerMoney =
+      current > ticketCost ? sellerMoney - current : sellerMoney + current;
+    if (sellerMoney < 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
